4d546fe8be12cdb01765c76694b5274d9d79039c
Arina Sanatulina <arinka295590@gmail.com>
2022-05-07 18:06:45 +0300

    added "Arina is a great student"

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -8,6 +8,8 @@ int main()
     using namespace boost::lambda;
     typedef std::istream_iterator<int> in;
 
+
+    std::cout << ("Arina is a great student that is how many times:");
     std::for_each(
         in(std::cin), in(), std::cout << (_1 * 5) << " ");
 }
