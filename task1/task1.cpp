7c9c243c2aa5005a6975693cf9f4e91bfaa9cb81
Arina Sanatulina <arinka295590@gmail.com>
2022-05-07 19:04:34 +0300

    merging...

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -8,8 +8,11 @@ int main()
     using namespace boost::lambda;
     typedef std::istream_iterator<int> in;
 
+<<<<<<< HEAD
     std::cout << ("The main point is...");
     std::cout << ("w8 for a few seconds...");
+=======
+>>>>>>> 1b54524 (some part deleted)
     std::cout << ("Arina and her groupmates are great students that is how many times:");
     std::for_each(
         in(std::cin), in(), std::cout << "  Or so many: " << (_1 * 100)<< " \n  ");
