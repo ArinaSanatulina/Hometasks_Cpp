a6c0c9af30ef9de57d560c9a0228ca74945b31fc
Arina Sanatulina <arinka295590@gmail.com>
2022-05-07 18:32:01 +0300

    multiplying by 100, not 5

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -8,9 +8,9 @@ int main()
     using namespace boost::lambda;
     typedef std::istream_iterator<int> in;
 
-
+    
     std::cout << ("Arina and her groupmates are great students that is how many times:");
     std::for_each(
-        in(std::cin), in(), std::cout << "  Or so many: " << (_1 * 5)<< " \n  ");
+        in(std::cin), in(), std::cout << "  Or so many: " << (_1 * 100)<< " \n  ");
 
 }
