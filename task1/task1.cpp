c7e9ac3fddbf3b00fd93ea19c62942816de803ff
Arina Sanatulina <arinka295590@gmail.com>
2022-05-07 19:01:40 +0300

    added one sentence

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -8,15 +8,8 @@ int main()
     using namespace boost::lambda;
     typedef std::istream_iterator<int> in;
 
-<<<<<<< HEAD
-    
-=======
-<<<<<<< HEAD
     std::cout << ("The main point is...");
-=======
-    
->>>>>>> main
->>>>>>> branch1
+    std::cout << ("w8 for a few seconds...");
     std::cout << ("Arina and her groupmates are great students that is how many times:");
     std::for_each(
         in(std::cin), in(), std::cout << "  Or so many: " << (_1 * 100)<< " \n  ");
